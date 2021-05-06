864bda4116b3dd1f4f17dc40db381cd75f9c70d7
Filip Lipowiecki <filip.lipowiecki@microsoft.wsei.edu.pl>
2021-05-06 19:53:59 +0200

    initial commit

---
+++ DesignPatternsExamples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesignPatternsExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
